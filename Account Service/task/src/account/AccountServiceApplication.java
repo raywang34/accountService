d4e0cb127e19f15f7a4caab7ceca73bde83f5c9f
Ray Wang <raywangmal3082@gmail.com>
2022-05-20 16:07:01 +0800

    Unit test by Mock

--- Account Service/task/src/account/AccountServiceApplication.java
+++ Account Service/task/src/account/AccountServiceApplication.java
@@ -3,6 +3,8 @@ package account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 /**
  * @author Ray
  */
@@ -11,5 +13,34 @@ public class AccountServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> hashMap = new HashMap<>();
+
+        List<String> tmpList;
+
+        char[] strtoChar;
+
+        String sortedStr;
+
+        for (String str : strs) {
+            strtoChar = str.toCharArray();
+            Arrays.sort(strtoChar);
+            sortedStr = String.valueOf(strtoChar);
+
+            if (hashMap.get(sortedStr) != null) {
+                tmpList = hashMap.get(sortedStr);
+                tmpList.add(str);
+                hashMap.put(sortedStr, tmpList);
+            } else {
+                tmpList = new ArrayList<>();
+                tmpList.add(str);
+                hashMap.put(sortedStr, tmpList);
+            }
+        }
+
+        return new ArrayList<>(hashMap.values());
     }
 }
